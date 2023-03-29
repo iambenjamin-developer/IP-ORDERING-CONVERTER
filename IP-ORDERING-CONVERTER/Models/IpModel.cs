4780357c9ccbb2a1a07661a90c80e42910c77f8a
Benjamin Correa <iambenjamin.developer@gmail.com>
2023-03-29 10:18:23 -0300

    agregar modelo

---
+++ IP-ORDERING-CONVERTER/Models/IpModel.cs
@@ -0,0 +1,9 @@
+﻿namespace IP_ORDERING_CONVERTER.Models
+{
+    public class IpModel
+    {
+        public long Index { get; set; }
+        public string IP { get; set; }
+        public long DecimalIP { get; set; }
+    }
+}
--- IP-ORDERING-CONVERTER/Program.cs
+++ IP-ORDERING-CONVERTER/Program.cs
@@ -1,35 +1,36 @@
 ﻿using System.Net;
 
-
-internal class Program
+namespace IP_ORDERING_CONVERTER
 {
-    static void Main(string[] args)
+    internal class Program
     {
-        Console.WriteLine(ToInt("10.250.24.7"));
-        Console.WriteLine(ToAddr(134744072));
-    }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ToInt("10.250.24.7"));
+            Console.WriteLine(ToAddr(134744072));
+        }
 
-    static long ToInt(string addr)
-    {
-        // careful of sign extension: convert to uint first;
-        // unsigned NetworkToHostOrder ought to be provided.
-        return (long)(uint)IPAddress.NetworkToHostOrder(
-             (int)IPAddress.Parse(addr).Address);
-    }
+        static long ToInt(string addr)
+        {
+            // careful of sign extension: convert to uint first;
+            // unsigned NetworkToHostOrder ought to be provided.
+            return (long)(uint)IPAddress.NetworkToHostOrder(
+                 (int)IPAddress.Parse(addr).Address);
+        }
 
-    static string ToAddr(long address)
-    {
-        return IPAddress.Parse(address.ToString()).ToString();
-        // This also works:
-        // return new IPAddress((uint) IPAddress.HostToNetworkOrder(
-        //    (int) address)).ToString();
-    }
+        static string ToAddr(long address)
+        {
+            return IPAddress.Parse(address.ToString()).ToString();
+            // This also works:
+            // return new IPAddress((uint) IPAddress.HostToNetworkOrder(
+            //    (int) address)).ToString();
+        }
 
-    public void OrdenarIpsString()
-    {
-        var unsortedIps =
-                    new[]
-                    {
+        public void OrdenarIpsString()
+        {
+            var unsortedIps =
+                        new[]
+                        {
                                     "192.168.1.4",
                                     "192.168.1.5",
                                     "132.26.211.21",
@@ -39,18 +40,19 @@ internal class Program
                                     "192.168.2.1",
                                     "10.152.16.23",
                                     "69.52.220.44"
-                    };
+                        };
 
-        var sortedIps = unsortedIps
-            .Select(Version.Parse)
-            .OrderBy(arg => arg)
-            .Select(arg => arg.ToString())
-            .ToList();
+            var sortedIps = unsortedIps
+                .Select(Version.Parse)
+                .OrderBy(arg => arg)
+                .Select(arg => arg.ToString())
+                .ToList();
 
-        foreach (var ip in sortedIps)
-        {
-            Console.WriteLine(ip);
+            foreach (var ip in sortedIps)
+            {
+                Console.WriteLine(ip);
+            }
+
         }
-
     }
 }
